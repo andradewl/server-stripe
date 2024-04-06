98549f25a1dea92e549043c04d94e8e2654fe8a3
andradewl <andradeweblab@gmail.com>
2024-04-06 01:54:58 -0600

    cambios index

--- index.js
+++ index.js
@@ -1,67 +1,43 @@
-import express from "express";
-import { static as expressStatic } from "express";
-import { resolve } from "path";
-import dotenv from "dotenv";
-import stripePackage from "stripe"; // Import stripe using ES module syntax
-import cors from "cors"; // Import cors
+const stripe = require('stripe')('sk_test_51O7LnKDtJXAsj9ndOjlcgKH1U5u5PJiuur3iPMNb8chhE6VcpscyDtBdzly0yvaoUK4u1xOoQJuSV5MuQuihLduO00AbluE6H0');
 
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-dotenv.config({ path: "./.env" });
+app.use(bodyParser.json())
 
-// Initialize stripe with the secret key
-const stripe = stripePackage(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-08-01",
+
+process.on('uncaughtException', function (err) {
+  console.log(err);
 });
 
-app.use(expressStatic(process.env.STATIC_DIR));
-
-app.use(cors());
-
-
-// app.get("/", (req, res) => {
-//   const path = resolve(process.env.STATIC_DIR + "/index.html");
-//   res.sendFile(path);
-// });
+app.get('/',(req, res)=>{
+    res.send("Hello Folks..!!! Please subscribe my channel")
+})
 
 
-app.get("/", (req, res) => {
-  res.send("<h1>¡Hola!</h1>");
-});
+app.post('/payment-sheet', async (req, res) => {
+    // Use an existing Customer ID if this is a returning customer.
 
-app.get("/config", (req, res) => {
-  res.send({
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  });
-});
+    const {amount, currency} = req.body
 
-app.post("/create-payment-intent", async (req, res) => {
-  try {
-
-    // const { payment_method_config_id } = req.body; // Obtener el ID de la configuración del método de pago del cuerpo de la solicitud
-    // if (!payment_method_config_id) {
-    //   throw new Error("ID de la configuración del método de pago no proporcionado");
-    // }
-
+    const customer = await stripe.customers.create();
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+      {customer: customer.id},
+      {apiVersion: '2022-08-01'}
+    );
     const paymentIntent = await stripe.paymentIntents.create({
-      currency: "EUR",
-      amount: 1999,
-      // payment_method_types: ["card"],
-      // payment_method_config: payment_method_config_id,
-      automatic_payment_methods: { enabled: true },
+      amount: amount,
+      currency: currency,
+      customer: customer.id,
+      payment_method_types: [ 'card'],
     });
+  
+    res.json({
+      paymentIntent: paymentIntent.client_secret,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer.id,
+    });
+  });
 
-    // Send publishable key and PaymentIntent details to client
-    res.send({
-      clientSecret: paymentIntent.client_secret,
-    });
-  } catch (e) {
-    return res.status(400).send({
-      error: {
-        message: e.message,
-      },
-    });
-  }
-});
-
-app.listen(4002, () => console.log("Running on http://localhost:4002"));
+app.listen(4002, ()=> console.log("Running on http://localhost:4002"))
